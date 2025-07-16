ce975bd1838a20c6aef70991de5d5a1c919cdc29
BenComer09 <thelorant@gmail.com>
2025-07-16 15:04:31 -0700

    Basic set-up

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import sqlite3
+
+app = Flask(__name__)
+CORS(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
